ef22999de64be07909bdbae255fd3a24ffa69f43
OggyShan <134085794+OggyShan@users.noreply.github.com>
2023-10-07 09:36:46 +0530

    Update Chatwindow.xaml.cs
    initialized python and startup location changed

--- Oasis/Chatwindow.xaml.cs
+++ Oasis/Chatwindow.xaml.cs
@@ -11,19 +11,34 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Python.Runtime;
 
 namespace Oasis
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+   public partial class ChatWindow : Window
+   {
+    public ChatWindow()
     {
-        public Window1()
+        if (!PythonEngine.IsInitialized)
         {
-            InitializeComponent();
-            this.WindowStartupLocation = WindowStartupLocation.Manual;
-            this.ResizeMode=ResizeMode.NoResize;
+            Runtime.PythonDLL = @"C:\Users\Lenovo\AppData\Local\Programs\Python\Python39\python39.dll";
+            PythonEngine.Initialize();
         }
+        InitializeComponent();
+        double height = SystemParameters.PrimaryScreenHeight;
+        double width = SystemParameters.PrimaryScreenWidth;
+        double chat_height = (65 * height) / 100;
+        double chat_width = (25 * width) / 100;
+        this.WindowStartupLocation = WindowStartupLocation.Manual;
+        this.ResizeMode=ResizeMode.NoResize;
+        this.Height = chat_height;
+        this.Width = chat_width;
+        this.Left = (width - chat_width)+((1.5*Width)/100);
+        this.Top = ((height - chat_height)-((4.7* height)/100));
+
     }
+   }
 }
